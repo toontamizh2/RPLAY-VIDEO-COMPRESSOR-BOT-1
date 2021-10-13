fef400af9457170f1f62814cbb3eb6c40c5c5110
RplayRENISH <88858106+Rplayoriginal@users.noreply.github.com>
2021-10-13 23:16:28 +0100

    Update funcn.py

--- helper/funcn.py
+++ helper/funcn.py
@@ -18,7 +18,7 @@ COUNT = []
 OK = {}
 
 uptime = dt.now()
-os.system("wget https://telegra.ph/file/75ee20ec8d8c8bba84f02.jpg -O thumb.jpg")
+os.system("wget https://telegra.ph/file/908e8d3dcbd9704d22a3b.jpg -O thumb.jpg")
 
 if not os.path.isdir("downloads/"):
     os.mkdir("downloads/")
