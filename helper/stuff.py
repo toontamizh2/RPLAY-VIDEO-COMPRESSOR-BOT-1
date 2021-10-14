5c1b1af95988d03ff3b4abeaf0f341a91c0ea4d4
RplayRENISH <88858106+Rplayoriginal@users.noreply.github.com>
2021-10-14 01:13:39 +0100

    Update stuff.py

--- helper/stuff.py
+++ helper/stuff.py
@@ -158,10 +158,10 @@ async def ccom(e):
         reply = cv.wait_event(events.NewMessage(from_users=chat))
         repl = await reply
         if "." in repl.text:
-            q = repl.text.split(".")[-1]
-            g = repl.text.replace(q, "@R.Play™✓", "mkv")
+            q = repl.text.split(".")[-1]("@R.Play™✓")[+1]
+            g = repl.text.replace(q, "mkv")
         else:
-            g = repl.text + "@R.Play™✓" ,".mkv"
+            g = repl.text + ".mkv"
         outt = f"encode/{chat}/{g}"
         x = await repl.reply(
             f"Custom File Name : {g}\n\nSend Thumbnail Picture For it."
