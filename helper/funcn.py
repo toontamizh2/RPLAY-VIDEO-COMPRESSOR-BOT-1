98f2f3a13f88d75f3e6a531046ecbf2004763565
RplayRENISH <88858106+Rplayoriginal@users.noreply.github.com>
2021-10-21 23:41:06 +0100

    Update funcn.py

--- helper/funcn.py
+++ helper/funcn.py
@@ -75,18 +75,18 @@ def hbs(size):
 async def progress(current, total, event, start, type_of_ps, file=None):
     now = time.time()
     diff = now - start
-    if round(diff % 10.00) == 0 or current == total:
+    if round(diff % 3.00) == 0 or current == total:
         percentage = current * 100 / total
         speed = current / diff
         time_to_completion = round((total - current) / speed) * 1000
         progress_str = "`[{0}{1}] {2}%`\n\n".format(
-            "".join(["●" for i in range(math.floor(percentage / 5))]),
-            "".join(["○" for i in range(20 - math.floor(percentage / 5))]),
+            "".join(["🟩" for i in range(math.floor(percentage / 5))]),
+            "".join(["⬛" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2),
         )
         tmp = (
             progress_str
-            + "`{0} of {1}`\n\n`✦ Speed: {2}/s`\n\n`✦ ETA: {3}`\n\n".format(
+            + "🌟`{0} of {1}`\n\n`🌟 Speed: {2}/s`\n\n`🌟 ETA: {3}`\n\n".format(
                 hbs(current),
                 hbs(total),
                 hbs(speed),
@@ -95,10 +95,10 @@ async def progress(current, total, event, start, type_of_ps, file=None):
         )
         if file:
             await event.edit(
-                "`✦ {}`\n\n`File Name: {}`\n\n{}".format(type_of_ps, file, tmp)
+                "`🌟 {}`\n\n`File Name: {}`\n\n{}".format(type_of_ps, file, tmp)
             )
         else:
-            await event.edit("`✦ {}`\n\n{}".format(type_of_ps, tmp))
+            await event.edit("`🌟 {}`\n\n{}".format(type_of_ps, tmp))
 
 
 async def genss(file):
