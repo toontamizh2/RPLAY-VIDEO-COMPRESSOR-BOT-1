64f49d0c5fe956533e7898c128eb38bcd64bab67
RplayRENISH <88858106+Rplayoriginal@users.noreply.github.com>
2021-10-21 21:42:10 +0100

    Update worker.py

--- helper/worker.py
+++ helper/worker.py
@@ -109,7 +109,10 @@ async def encc(e):
         ds =  await e.client.send_file(
             e.chat_id,
             file=ok,
+            metadata = 0,
             duration = 0,
+            width = 0,
+            height = 0,
             caption=f"video compressed by @Rplay_compressor_bot\n\n\nMade by @renishrplay\n`RENISH`\n`હા હું ગુજરતી.`\n**રેનીશ**",
             force_document=False,
             thumb=take_screen_shot(ok, os.path.dirname(os.path.abspath(ok)), random.randint(0, duration - 1)),
